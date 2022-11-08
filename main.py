39f44e17596cd62b6c1ece2e29c9ad3509482839
Trent Hawkins <spcom1024@gmail.com>
2022-11-08 16:11:12 +0200

    Start `main` script

---
+++ main.py
@@ -0,0 +1,23 @@
+"""Main file for setting up and performing experiments."""
+
+
+import tensorflow
+
+import source.dataset.animals_with_attributes as AA
+import source.zeroshot.embedding as G0
+
+
+def read_conf(conf_file: str):
+	"""Read `.json` configuration file with keyword arguments.
+
+	The configuration format:
+	```json
+	{
+	}
+	```
+	"""
+	...
+
+
+if __name__ == "__main__":
+	...
