a0ec9bfa3c6bdd8fbc291ba161e66d4c98bf5769
Trent Hawkins <spcom1024@gmail.com>
2022-11-11 17:39:52 +0200

    Clean-up classifiers test

--- source/zeroshot/test_zeroshot.py
+++ source/zeroshot/test_zeroshot.py
@@ -1,13 +1,21 @@
 """Tests for models on zeroshot problems defined here."""
 
 
-class TestEfficientNetDense:
+class TestModels:
 	"""Tests for specific latent-embedding model based on EfficientNet and Dense."""
 
-	def test_models(self):
-		"""Test proper instantiation of model."""
-		from random import randint
+	from random import randint
 
+	batch_size = randint(1, 50)
+	image_size = (
+		224,
+		224, 3
+	)
+
+	input_shape = (batch_size, *image_size)
+
+	def test_efficientnet_dense(self):
+		"""Test proper model instantiation and output."""
 		import numpy
 		import tensorflow
 
@@ -17,13 +25,7 @@ class TestEfficientNetDense:
 		from source.keras.layers import JaccardDense
 		from source.zeroshot.models import EfficientNetDense
 
-		batch_size = randint(1, 50)
-		image_size = (
-			224,
-			224, 3
-		)
-
-		images = tensorflow.random.uniform((batch_size, *image_size),
+		images = tensorflow.random.uniform(self.input_shape,
 			minval=0,
 			maxval=255,
 			dtype=tensorflow.int64,
@@ -40,12 +42,12 @@ class TestEfficientNetDense:
 			semantic_class=JaccardDense,
 		)
 
-	#   Assert semantic weight are properly initialized
+	#   Assert semantic weight are properly initialized:
 		assert numpy.allclose(softmaxModel.layers[-1].kernel.numpy(), predicates)
 		assert numpy.allclose(jaccardModel.layers[-1].kernel.numpy(), predicates)
 
 	#	Assert softmax output:
-		assert tensorflow.reduce_sum(softmaxModel(images)) == batch_size
+		assert tensorflow.reduce_sum(softmaxModel(images)) == self.batch_size
 
 	#	Assert sigmoid-like output:
 		assert tensorflow.math.reduce_all(0 <= jaccardModel(images))  # type: ignore
