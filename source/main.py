ec8684e1ed282065fe175526ffc162f9c70d73f9
Trent Hawkins <spcom1024@gmail.com>
2022-11-14 00:02:57 +0200

    Move `main.py` to `source`

--- source/main.py
+++ source/main.py
@@ -1,4 +1,28 @@
-"""Main."""
+#!/bin/python3 --
+
+
+"""A sample learning cycle.
+
+Initialize a dataset, setting all its metadata necessary to generate splits.
+
+Initialize a model with the defined components:
+-	Choose a visual component, in this case an EfficientNet model.
+-	Choose a predicate matrix from the dataset to use as kernel for the semantic component.
+-	Choose (optionally) a type of semantic component (to potentially use manifestly non-linear layers).
+
+Initialize a classifier with said dataset and model:
+-	Compile it with its predefined:
+	-	optimizer
+	-	loss
+	-	metrics
+-	Summarize model to see if things go according to plan.
+
+Perform learning cycle:
+-	Fit the model on training data as set by the dataset.
+-	Generate predictions on test data as set by the dataset, to be used in generating confusion plots.
+-	Test model on predefined metrics.
+"""
+
 
 import os; os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow
@@ -12,7 +36,7 @@ from source.zeroshot.classifiers import QuasifullyZeroshotCategoricalClassifier
 from source.zeroshot.models import EfficientNetDense
 
 if __name__ == "__main__":
-	"""Learning cycle."""
+	"""This will become the main executable script where experiments will be loaded."""
 
 #	Setup data:
 	dataset = Dataset()
