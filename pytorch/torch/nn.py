cbc19d5644b834283cc5aac0f1ed3dc9d63e03a7
Trent Hawkins <spcom1024@gmail.com>
2023-07-09 17:21:30 +0300

    Finish custom layers in `torch.nn`

--- pytorch/torch/nn.py
+++ pytorch/torch/nn.py
@@ -10,30 +10,29 @@ Metric sigmoid-like non-linear dense layers using similarity metrics in their de
 -	based on the jaccard similarity metric (sigmoid-like only on sigmoid-like features)
 -	based on the cosine similarity metric (sigmoid-like always)
 
-Dense layer stack.
+Linear layer stack.
 -	Basically a sequential of dense layers with uniform settings across, apart for the top.
 
-Dense layer stack array.
+Linear layer stack array.
 -	An colelction of dense sequentials used in parallel.
 """
 
 from __future__ import annotations
 
-from typing import Callable, Optional, Union
-
-import tensorflow
+import scipy.special
 import torch
 
 
-class DropoutDense(torch.nn.Linear):
+class DropoutLinear(torch.nn.Linear):
 	"""Custom compound dense layer.
 
 	Attribures:
 		dense: the dense sublayer
 	"""
 
-	def __init__(self, input_size: int, output_size: int,
-		dropout_rate: float = .5,
+	def __init__(self,
+		inputs_shape: int,
+		output_shape: int, dropout_rate: float = .0,
 	**kwargs):
 		"""Hyrparametrize base layer with dense topping.
 
@@ -41,21 +40,21 @@ class DropoutDense(torch.nn.Linear):
 			units: number of neurons in layer
 
 		Keyword arguments:
-			dropout: dropout factor applied on input of the layer
+			dropout: logit of dropout factor applied on input of the layer
 				default: half
 		"""
-		super(DropoutDense, self).__init__(input_size, output_size,
-		#	bias=True,
+		super(DropoutLinear, self).__init__(
+			inputs_shape,
+			output_shape, bias=True,
 		**kwargs)
 
 	#	dropout:
-		assert dropout_rate >= 0. and dropout_rate <= 1.
-		self.dropout = torch.nn.Dropout(dropout_rate)
+		self.dropout = torch.nn.Dropout(scipy.special.expit(dropout_rate))
 
 	#	activation:
-		self.activation = torch.nn.SiLU()
+		self.activation = torch.nn.SiLU()  # hardcoded best internal neuron activator
 
-	def forward(self, x: torch.Tensor) -> torch.Tensor:
+	def forward(self, inputs: torch.Tensor) -> torch.Tensor:
 		"""Call the model on new inputs.
 
 		In this case call just reapplies all ops in the graph to the new inputs.
@@ -66,17 +65,19 @@ class DropoutDense(torch.nn.Linear):
 		Returns:
 			output of layer
 		"""
-		return self.activation(super(DropoutDense, self).forward(self.dropout(x)))
+		return self.activation(super(DropoutLinear, self).forward(self.dropout(inputs)))
 
 
 """What follows are special types of dense layers."""
 
 
-class AttentionDense(torch.nn.Linear):
+class AttentionLinear(torch.nn.Linear):
 	"""Wrapper for dense layer operating on a stacks of input to recombine them with attention.
 
 	Such a layer is expected to have no bias and be trainable with no dropout.
 	Other dense features include activation only.
+	Make sure the batch dimensions include the threads themselves:
+		`len(shape(batch)) > 1`
 
 	Call:
 		stack: stack inputs horizontally
@@ -90,11 +91,11 @@ class AttentionDense(torch.nn.Linear):
 		Keyword arguments:
 			activation: to apply on output of decision
 		"""
-		super(AttentionDense, self).__init__(threads, 1,
-			bias=False,
-		**kwargs)
+		super(AttentionLinear, self).__init__(threads, 1, bias=False, **kwargs)
 
-	def call(self, x: torch.Tensor) -> torch.Tensor:
+		self.activation = torch.nn.Sigmoid()  # hardcoded semantic features renormalization
+
+	def call(self, inputs: torch.Tensor) -> torch.Tensor:
 		"""Call the model on new inputs.
 
 		In this case call just reapplies all ops in the graph to the new inputs.
@@ -105,63 +106,42 @@ class AttentionDense(torch.nn.Linear):
 		Returns:
 			output of layer
 		"""
-		return super(AttentionDense, self).forward(x).squeeze(
-			dim=-1,
-		)
+		return self.activation(super(AttentionLinear, self).forward(inputs).squeeze(dim=-1))
 
 
-class MetricDense(tensorflow.keras.layers.Dense):
+class MetricLinear(torch.nn.Linear):
 	"""A non-linear dense layer emulating the action of a metric and outputing in sigmoid range.
 
 	Such a modified layer has no bias explicitely.
 	"""
 
-	def __init__(self, units,
-		activation: Optional[Union[Callable, str]] = None,
-		kernel: Optional[tensorflow.Tensor] = None,
-		name: str = "metric",
-	**kwargs):
+	def __init__(self, kernel: torch.Tensor, **kwargs):
 		"""Hyperparametrize recombination layer.
 
 		No activation is needed as these metric layers are manifestly non-linear to begin with.
 		Option is left (and ignored) for compatibility with other dense-like layers.
 
-		Keyword arguments:
-			activation: to apply on output of decision
+		Arguments:
 			kernel: weight values to begin with
 		"""
-		super(MetricDense, self).__init__(kwargs.pop("units", None) or units,
-			activation=kwargs.pop("activation", None) or activation,
-			use_bias=kwargs.pop("use_bias", None) or False,
-		#	kernel_initializer=tensorflow.keras.initializers.Constant(kernel),  # type: ignore
-		#	bias_initializer="zeros",
-		#	kernel_regularizer=regularizer,
-		#	bias_regularizer=regularizer,
-		#	activity_regularizer=regularizer,
-		#	kernel_constraint=constraint,
-		#	bias_constraint=constraint,
-			name=kwargs.pop("name", None) or name,
-		**kwargs)
+		super(MetricLinear, self).__init__(*kernel.size(), bias=False, **kwargs)
 
-		self.initial_kernel = kernel
+	#	frozen semantic kernel:
+		assert self.weight.dim() == kernel.transpose(0, 1).dim()
+		self.weight = kernel.transpose(0, 1)
+		self.weight.requires_grad = False
 
-	def build(self, input_shape: tensorflow.TensorShape):
-		"""Custtomize kernel."""
-		super(MetricDense, self).build(input_shape)
-
-		kernel, *weights = self.get_weights()
-
-		if self.initial_kernel is not None and kernel.shape == self.initial_kernel.shape:
-			self.set_weights([self.initial_kernel, *weights])
+	#	probabilistic activation:
+		self.activation = torch.nn.Softmax()
 
 
-class CosineDense(MetricDense):
+class CosineLinear(MetricLinear):
 	"""A dense layer that perform the cosine operation per input and kernel vector instead of a dot product.
 
 	Such a modified layer has no bias explicitely.
 	"""
 
-	def call(self, inputs):
+	def forward(self, inputs):
 		"""Call the model on new inputs.
 
 		In this case call just reapplies all ops in the graph to the new inputs.
@@ -172,34 +152,24 @@ class CosineDense(MetricDense):
 		Returns:
 			output of layer
 		"""
-		inputs_kernel = super(CosineDense, self).call(inputs)
+		inputs_kernel = super(CosineLinear, self).forward(inputs)
 
 	#	the norms of inputs vectors:
-		inputs_inputs = tensorflow.expand_dims(
-			tensorflow.einsum("...i, ...i -> ...",
-				inputs,
-				inputs,
-			), -1
-		)
+		inputs_inputs = torch.einsum("...i, ...i -> ...",
+			inputs,
+			inputs,
+		).unsqueeze(-1)
 
 	#	the norms of kernel vectors (diagonal):
-		kernel_kernel = tensorflow.broadcast_to(
-			tensorflow.einsum("...ji, ...ji -> ...i",
-				self.kernel,
-				self.kernel,
-			), tensorflow.shape(inputs_kernel)
-		)
+		kernel_kernel = torch.einsum("...ji, ...ji -> ...i",
+			self.weight,
+			self.weight,
+		).expand(inputs_kernel.size())
 
-	#	denominator:
-		denominator = tensorflow.math.sqrt(
-			inputs_inputs *
-			kernel_kernel
-		)
-
-		return inputs_kernel / denominator
+		return inputs_kernel / torch.sqrt(inputs_inputs * kernel_kernel)
 
 
-class JaccardDense(MetricDense):
+class JaccardLinear(MetricLinear):
 	"""A dense layer that perform the Jaccard operation per input and kernel vector instead of a dot product.
 
 	Such a modified layer has no bias explicitely.
@@ -216,35 +186,24 @@ class JaccardDense(MetricDense):
 		Returns:
 			output of layer
 		"""
-		inputs_kernel = super(JaccardDense, self).call(inputs)
+		inputs_kernel = super(JaccardLinear, self).forward(inputs)
 
 	#	the norms of inputs vectors:
-		inputs_inputs = tensorflow.expand_dims(
-			tensorflow.einsum("...i, ...i -> ...",
-				inputs,
-				inputs,
-			), -1
-		)
+		inputs_inputs = torch.einsum("...i, ...i -> ...",
+			inputs,
+			inputs,
+		).unsqueeze(-1)
 
 	#	the norms of kernel vectors (diagonal):
-		kernel_kernel = tensorflow.broadcast_to(
-			tensorflow.einsum("...ji, ...ji -> ...i",
-				self.kernel,
-				self.kernel,
-			), tensorflow.shape(inputs_kernel)
-		)
+		kernel_kernel = torch.einsum("...ji, ...ji -> ...i",
+			self.weight,
+			self.weight,
+		).expand(inputs_kernel.size())
 
-	#	denominator:
-		denominator = (
-			inputs_inputs +
-			kernel_kernel -
-			inputs_kernel
-		)
-
-		return inputs_kernel / denominator
+		return inputs_kernel / (inputs_inputs + kernel_kernel - inputs_kernel)
 
 
-class DiceDense(MetricDense):
+class DiceLinear(MetricLinear):
 	"""A dense layer that perform the Dice operation per input and kernel vector instead of a dot product.
 
 	Such a modified layer has no bias explicitely.
@@ -261,81 +220,18 @@ class DiceDense(MetricDense):
 		Returns:
 			output of layer
 		"""
-		inputs_kernel = super(DiceDense, self).call(inputs)
+		inputs_kernel = super(DiceLinear, self).forward(inputs)
 
 	#	the norms of inputs vectors:
-		inputs_inputs = tensorflow.expand_dims(
-			tensorflow.einsum("...i, ...i -> ...",
-				inputs,
-				inputs,
-			), -1
-		)
+		inputs_inputs = torch.einsum("...i, ...i -> ...",
+			inputs,
+			inputs,
+		).unsqueeze(-1)
 
 	#	the norms of kernel vectors (diagonal):
-		kernel_kernel = tensorflow.broadcast_to(
-			tensorflow.einsum("...ji, ...ji -> ...i",
-				self.kernel,
-				self.kernel,
-			), tensorflow.shape(inputs_kernel)
-		)
+		kernel_kernel = torch.einsum("...ji, ...ji -> ...i",
+			self.weight,
+			self.weight,
+		).expand(inputs_kernel.size())
 
-	#	denominator:
-		denominator = (
-			inputs_inputs +
-			kernel_kernel
-		) / 2
-
-		return inputs_kernel / denominator
-
-
-class RandDense(MetricDense):
-	"""A dense layer that perform the Rand operation per input and kernel vector instead of a dot product.
-
-	Such a modified layer has no bias explicitely.
-	"""
-
-	def call(self, inputs):
-		"""Call the model on new inputs.
-
-		In this case call just reapplies all ops in the graph to the new inputs.
-
-		Arguments:
-			inputs: a tensor or list of tensors
-
-		Returns:
-			output of layer
-		"""
-		inputs_kernel = super(RandDense, self).call(inputs)
-		inputs_kernel_complement = 1 - inputs_kernel
-
-	#	the norms of inputs vectors:
-		inputs_inputs = tensorflow.expand_dims(
-			tensorflow.einsum("...i, ...i -> ...",
-				inputs,
-				inputs,
-			), -1
-		)
-
-	#	the norms of kernel vectors (diagonal):
-		kernel_kernel = tensorflow.broadcast_to(
-			tensorflow.einsum("...ji, ...ji -> ...i",
-				self.kernel,
-				self.kernel,
-			), tensorflow.shape(inputs_kernel)
-		)
-
-	#	numerator:
-		numerator = (
-			inputs_kernel +
-			inputs_kernel_complement
-		)
-
-	#	denominator:
-		denominator = (
-			inputs_inputs +
-			kernel_kernel -
-			inputs_kernel +
-			inputs_kernel_complement
-		)
-
-		return numerator / denominator
+		return inputs_kernel / ((inputs_inputs + kernel_kernel) / 2)
